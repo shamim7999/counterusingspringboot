560c4e2747bd19f7949e578759ef6058d05fa830
shamim7999 <sarker.shamim1998@gmail.com>
2024-01-28 23:50:53 +0600

    first commit

---
+++ src/main/java/com/request/counter/CounterusingspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.request.counter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CounterusingspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CounterusingspringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/request/counter/CounterusingspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.request.counter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounterusingspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
